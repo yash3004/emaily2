0d7f2158d680517bbad59cd2c8a0ab6da9b8af96
yash3004 <yashsehgal9253@gmail.com>
2023-08-28 21:02:11 +0530

    pathcer

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 app = express()
 app.get('/' , (req , res) => {
-    res.send({hi : "there"});
+    res.send({hi : "there mother fuxker !!"});
 });
 const PORT = process.env.PORT || 5100;
 app.listen(PORT);
